af10a66e79a49f660e2411428f52677aba788b0a
Sneh Shrivastava <subham4shrivastava@gmail.com>
2025-02-25 13:53:03 +0530

    fix resume viewer and resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Snehresume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -33,7 +34,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} renderTextLayer={false} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
